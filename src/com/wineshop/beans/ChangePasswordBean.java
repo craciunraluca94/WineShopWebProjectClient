4c13ce477fd4f41510ccf27fd7fb5416c817e37f
Raluca Craciun <craciun.raluca94@yahoo.ro>
2018-04-17 23:56:46 +0300

    Updated beans for login & changePassword.

--- src/com/wineshop/beans/ChangePasswordBean.java
+++ src/com/wineshop/beans/ChangePasswordBean.java
@@ -1,5 +1,9 @@
 package com.wineshop.beans;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -8,16 +12,45 @@ import javax.faces.context.FacesContext;
 
 import com.wineshop.dao.PlatformUserDAORemote;
 import com.wineshop.dto.ChangePasswordDTO;
+import com.wineshop.dto.PlatformUserDTO;
 import com.wineshop.exceptions.ChangePasswordException;
 
 @ManagedBean
 @RequestScoped
 public class ChangePasswordBean {
 
+	static final Logger LOGGER = Logger.getLogger(ChangePasswordBean.class.getName());
+
 	ChangePasswordDTO changePasswordDTO = new ChangePasswordDTO();
 
 	@EJB
 	PlatformUserDAORemote userDAORemote;
+
+	PlatformUserDTO userDTO;
+
+	@PostConstruct
+	public void init() {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		if (facesContext.getExternalContext().getFlash().get("user") != null) {
+			userDTO = (PlatformUserDTO) facesContext.getExternalContext().getFlash().get("user");
+		}
+	}
+
+	public String changePassword() {
+
+		LOGGER.log(Level.INFO, this.getClass().getName() + " Changing password: " + changePasswordDTO.toString());
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		try {
+			userDAORemote.updatePassword(changePasswordDTO);
+			facesContext.addMessage("changePassForm",
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "Password changed successfully!", null));
+			return "success";
+		} catch (ChangePasswordException e) {
+			facesContext.addMessage("changePassForm",
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), null));
+			return null;
+		}
+	}
 
 	public ChangePasswordDTO getChangePasswordDTO() {
 		return changePasswordDTO;
@@ -27,17 +60,11 @@ public class ChangePasswordBean {
 		this.changePasswordDTO = changePasswordDTO;
 	}
 
-	public String changePassword() {
-		FacesContext facesContext = FacesContext.getCurrentInstance();
-		try {
-			userDAORemote.updatePassword(changePasswordDTO);
-		} catch (ChangePasswordException e) {
-			facesContext.addMessage("changePassForm",
-					new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), null));
-			return null;
-		}
-		facesContext.addMessage("changePassForm",
-				new FacesMessage(FacesMessage.SEVERITY_INFO, "Update Successful", null));
-		return "success";
+	public PlatformUserDTO getUserDTO() {
+		return userDTO;
+	}
+
+	public void setUserDTO(PlatformUserDTO userDTO) {
+		this.userDTO = userDTO;
 	}
 }
--- src/com/wineshop/beans/LoginBean.java
+++ src/com/wineshop/beans/LoginBean.java
@@ -1,19 +1,29 @@
 package com.wineshop.beans;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
 import com.wineshop.dao.PlatformUserDAORemote;
 import com.wineshop.dto.LoginDTO;
+import com.wineshop.dto.PlatformUserDTO;
+import com.wineshop.exceptions.ChangePasswordException;
+import com.wineshop.exceptions.LoginException;
 
 @ManagedBean
 @RequestScoped
 public class LoginBean {
 
-	LoginDTO loginDTO;
+	static final Logger LOGGER = Logger.getLogger(LoginBean.class.getName());
+	LoginDTO loginDTO = new LoginDTO();
+	PlatformUserDTO platformUserDTO = new PlatformUserDTO();
 
 	@EJB
 	PlatformUserDAORemote userDAORemote;
@@ -22,11 +32,6 @@ public class LoginBean {
 	 * Default constructor.
 	 */
 	public LoginBean() {
-	}
-
-	@PostConstruct
-	public void init() {
-		loginDTO = new LoginDTO();
 	}
 
 	public LoginDTO getLoginDTO() {
@@ -38,8 +43,19 @@ public class LoginBean {
 	}
 
 	public String login() {
+		LOGGER.log(Level.INFO, "Trying to login with the credentials: ", loginDTO.toString());
 		FacesContext facesContext = FacesContext.getCurrentInstance();
-		System.out.println("Login: " + loginDTO.getUsername() + " " + loginDTO.getPassword());
-		return "";
+		try {
+			platformUserDTO = userDAORemote.login(loginDTO);
+			LOGGER.log(Level.INFO, "Successfully logged in with:" + loginDTO.toString());
+			facesContext.getExternalContext().getFlash().put("user", platformUserDTO);
+			facesContext.getExternalContext().redirect("changePassword.xhtml");
+			return "success";
+		} catch (LoginException | IOException e) {
+			if (e.getClass().equals(IOException.class))
+				LOGGER.log(Level.SEVERE, "An error occurred while redirecting to the change password page: ", e);
+			facesContext.addMessage("loginForm", new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), null));
+			return null;
+		}
 	}
 }
